c63710c56aefbbb8ab1607e278d143901955a567
bobby <bobby@gmail.com>
2024-09-05 09:53:32 +0530

    completed

---
+++ jexam1.java
@@ -0,0 +1,13 @@
+class jexam1
+{
+public static void main(String args[])
+{
+int a=5;
+if (a%2==0)
+{
+System.out.println("the given number is "+a+" its an even number");
+}
+else
+System.out.println("the given number is "+a+" its an odd number");
+}
+}
